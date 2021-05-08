d36464d68da8165c507007216c20cb9e0fa99240
Dillon Brock <dillonbrock@dillons-mbp.lan>
2021-05-08 12:22:48 -0500

    started body class, testing supershape as way to draw them

--- sketch.js
+++ sketch.js
@@ -1,10 +1,59 @@
+
+
+
 function setup() {
-  // put setup code here
-  createCanvas(800,800);
+  createCanvas(800,800, WEBGL);
 
 }
 
 function draw() {
-  // put drawing code here
-  ellipse(mouseX, mouseY, 200, 200);
+
+  background(0);
+
+}
+
+class Body {
+
+	constructor(x, y, z) {
+
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.a = 1;
+		this.b = 1;
+		this.m;
+		this.n1;
+		this.n2;
+		this.n3;
+		this.size = random()
+
+	}
+
+	display() {
+
+		push();
+
+
+		pop();
+
+	}
+
+	getSize() {
+
+		return this.size;
+
+	}
+
+	supershape(m, n1, n2, n3, theta) {
+
+		let t1 = abs((1/this.a)*cos(m * theta / 4));
+  		t1 = pow(t1, n2);
+  		let t2 = abs((1/this.b)*sin(m * theta/4));
+  		t2 = pow(t2, n3);
+  		let t3 = t1 + t2;
+  		let r = pow(t3, - 1 / n1);
+  		return r;
+
+	}
+
 }
